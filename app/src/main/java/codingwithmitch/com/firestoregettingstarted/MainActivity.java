aa30d522449dcfe0ced4b8b979ee5e079485a5c9
Mitch <mitch@tabian.ca>
2018-05-14 14:04:55 -0700

    testing pagination with RecyclerView

--- app/src/main/java/codingwithmitch/com/firestoregettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/firestoregettingstarted/MainActivity.java
@@ -4,20 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import codingwithmitch.com.firestoregettingstarted.models.Note;
 
 /**
  * Created by User on 5/14/2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,
+        IMainActivity
+{
 
     private static final String TAG = "MainActivity";
 
@@ -26,19 +47,159 @@ public class MainActivity extends AppCompatActivity {
 
 
     //widgets
-    
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
 
     //vars
+    private View mParentLayout;
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NoteRecyclerViewAdapter mNoteRecyclerViewAdapter;
+    private DocumentSnapshot mLastVisible;
+
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFab = findViewById(R.id.fab);
+        mParentLayout = findViewById(android.R.id.content);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+        mFab.setOnClickListener(this);
 
         setupFirebaseAuth();
+        initRecyclerView();
+        getNotes(null);
     }
 
+    private void getNotes(DocumentSnapshot lastVisible){
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        CollectionReference notesCollectionRef = db
+                .collection("notes");
+
+        Query notesQuery = null;
+        if(lastVisible != null){
+            notesQuery = notesCollectionRef
+                    .whereEqualTo("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .orderBy("timestamp", Query.Direction.ASCENDING)
+                    .startAfter(lastVisible)
+                    .limit(2);
+        }
+        else{
+            notesQuery = notesCollectionRef
+                    .whereEqualTo("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .orderBy("timestamp", Query.Direction.ASCENDING)
+                    .limit(2);
+        }
+
+
+        notesQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+
+                    if(mNotes.size() > 0){
+                        if(mNotes.get(mNotes.size() - 1).getTitle().equals("")){
+                            mNotes.remove(mNotes.size() - 1);
+                        }
+                    }
+
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        Note note = document.toObject(Note.class);
+                        mNotes.add(note);
+                    }
+
+                    if(task.getResult().size() != 0){
+                        // Get the last visible document
+                        mLastVisible = task.getResult().getDocuments()
+                                .get(task.getResult().size() -1);
+                    }
+
+                    mNoteRecyclerViewAdapter.notifyDataSetChanged();
+                }
+                else{
+                    makeSnackBarMessage("Query Failed. Check Logs.");
+                }
+            }
+        });
+    }
+
+    private void initRecyclerView(){
+        if(mNoteRecyclerViewAdapter == null){
+            mNoteRecyclerViewAdapter = new NoteRecyclerViewAdapter(mNotes);
+        }
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mNoteRecyclerViewAdapter);
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    Log.d(TAG, "onScrolled: List has reached the bottom. Getting more notes.");
+                    Note note = new Note();
+                    note.setTitle("");
+                    mNotes.add(note);
+                    mNoteRecyclerViewAdapter.notifyDataSetChanged();
+                    getNotes(mLastVisible);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void createNewNote(String title, String content) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference newNoteRef = db
+                .collection("notes")
+                .document();
+
+        Note note = new Note();
+        note.setTitle(title);
+        note.setContent(content);
+        note.setNote_id(newNoteRef.getId());
+        note.setUser_id(userId);
+
+        newNoteRef.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    makeSnackBarMessage("Created new note");
+                }
+                else{
+                    makeSnackBarMessage("Failed. Check log.");
+                }
+            }
+        });
+    }
+
+    private void makeSnackBarMessage(String message){
+        Snackbar.make(mParentLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.fab:{
+                //create a new note
+                NewNoteDialog dialog = new NewNoteDialog();
+                dialog.show(getSupportFragmentManager(), getString(R.string.dialog_new_note));
+                break;
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,7 +210,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
         switch (item.getItemId()){
             case R.id.optionSignOut:
                 signOut();
@@ -103,6 +263,9 @@ public class MainActivity extends AppCompatActivity {
             FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
         }
     }
+
+
+
 }
 
 
