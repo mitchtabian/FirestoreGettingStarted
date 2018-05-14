2f63fd566f9d9f47d9463f290c88a4642333ba66
Mitch <mitch@tabian.ca>
2018-05-14 12:35:08 -0700

    Inserting_Data_End

--- app/src/main/java/codingwithmitch/com/firestoregettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/firestoregettingstarted/MainActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import codingwithmitch.com.firestoregettingstarted.models.Note;
 
 /**
  * Created by User on 5/14/2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,
+        IMainActivity
+{
 
     private static final String TAG = "MainActivity";
 
@@ -26,19 +38,77 @@ public class MainActivity extends AppCompatActivity {
 
 
     //widgets
-    
+    private FloatingActionButton mFab;
 
     //vars
+    View mParentLayout;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFab = findViewById(R.id.fab);
+        mParentLayout = findViewById(android.R.id.content);
 
         setupFirebaseAuth();
+        mFab.setOnClickListener(this);
     }
 
+    @Override
+    public void createNewNote(String title, String content) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference newNoteRef = db
+                .collection("notes")
+                .document();
+
+        Note note = new Note();
+        note.setTitle(title);
+        note.setContent(content);
+        note.setNote_id(newNoteRef.getId());
+        note.setUser_id(userId);
+
+        newNoteRef.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    newNoteSuccess();
+                }
+                else{
+                    newNoteFailed();
+                }
+            }
+        });
+    }
+
+    private void newNoteFailed(){
+        Log.d(TAG, "newNoteSuccess: failed to create new note.");
+
+        Snackbar.make(mParentLayout, "Failed. Check log.", Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void newNoteSuccess(){
+        Log.d(TAG, "newNoteSuccess: inserted new note.");
+        Snackbar.make(mParentLayout, "Created new note", Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.fab:{
+                //create a new note
+                NewNoteDialog dialog = new NewNoteDialog();
+                dialog.show(getSupportFragmentManager(), getString(R.string.dialog_new_note));
+                break;
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,6 +173,9 @@ public class MainActivity extends AppCompatActivity {
             FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
         }
     }
+
+
+
 }
 
 
